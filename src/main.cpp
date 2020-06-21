0c83b604ef1cbb54397f72f7ca0ce2bd60e28e6c
Gogo <Gogo@DESKTOP-BNNHEEH>
2020-06-21 21:59:16 +0100

    draw triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include <GLFW/glfw3.h>
 #include "iostream"
 
-int main(void)
-{
-    GLFWwindow* window;
+int main(void) {
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -12,8 +11,7 @@ int main(void)
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!window) {
         glfwTerminate();
         return -1;
     }
@@ -22,18 +20,38 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     GLenum err = glewInit();
-    if (GLEW_OK != err)
-    {
+    if (GLEW_OK != err) {
         /* Problem: glewInit failed, something is seriously wrong. */
         std::cout << "Error: " << glewGetErrorString(err) << std::endl;
     }
     std::cout << "Status: Using GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
 
+    unsigned int VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const float g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+    };
+
+    unsigned int vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
