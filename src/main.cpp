c6afbb6d5d851cdda71d5ccac4b576e0f8d8ded4
Gogo <g.martsenkov@gmail.com>
2020-06-23 13:51:15 +0100

    render the same texture by using a second mvp

--- src/main.cpp
+++ src/main.cpp
@@ -47,10 +47,10 @@ int main() {
     std::cout << "Status: Using GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
 
     static const float g_vertex_buffer_data[] = {
-            100.0f, 100.0f, 0.0f, 0.0f,
-            200.0f, 100.0f, 1.0f, 0.0f,
-            200.0f, 200.0f, 1.0f, 1.0f,
-            100.0f, 200.0f, 0.0f, 1.0f
+            -50.0f, -50.0f, 0.0f, 0.0f,
+            50.0f, -50.0f, 1.0f, 0.0f,
+            50.0f, 50.0f, 1.0f, 1.0f,
+            -50.0f, 50.0f, 0.0f, 1.0f
     };
 
     static const unsigned int indices_buffer_data[] = {
@@ -71,16 +71,10 @@ int main() {
     IndexBuffer *ib = new IndexBuffer(indices_buffer_data, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-    glm::mat4 mvp = proj * view * model;
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
     Shader *shader = new Shader("../res/shaders/BasicVertex.shader", "../res/shaders/BasicFragment.shader");
     shader->Bind();
-    shader->SetUniform4f("u_Color", 1.0f, 0.3f, 0.8f, 1.0f);
-    shader->SetUniformMat4f("u_MVP", mvp);
-
 
     Texture* texture = new Texture("../res/textures/dog.png");
     texture->Bind();
@@ -98,12 +92,9 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
     ImGui::StyleColorsDark();
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    float r = 0.0f;
-    float increment = 0.05f;
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 200, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
@@ -114,35 +105,27 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        shader->Bind();
-        shader->SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-
-        if (r > 1.0f)
-            increment = -0.01f;
-        else if (r < 0.0f)
-            increment = 0.05;
-
-        r += increment;
-
-        renderer.Draw(*va, *ib, *shader);
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+            glm::mat4 mvp = proj * view * model;
+            shader->Bind();
+            shader->SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(*va, *ib, *shader);
+        }
 
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+            glm::mat4 mvp = proj * view * model;
+            shader->Bind();
+            shader->SetUniformMat4f("u_MVP", mvp);
+            renderer.Draw(*va, *ib, *shader);
+        }
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+        {
+            ImGui::Begin("Controls");
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
